e09b4cb42e7a3e79d8f31dfabafde9dc092b1b29
Alexandre Moreira <54758757+amoreira2003@users.noreply.github.com>
2021-01-29 05:49:32 -0300

    Now that’s a lot of damage
    In this first update, I implemented the tilemap system and a couple of test tiles that will assuredly change in a short future. I also added the main camera movement being Rotation, Zooming, and Mouse Movimentation.

---
+++ Assets/DragCamera.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragCamera : MonoBehaviour
+{
+
+
+ private Vector3 ResetCamera; // original camera position
+ private Vector3 Origin; // place where mouse is first pressed
+ private Vector3 Diference; // change in position of mouse relative to origin
+
+ public float smoothSpeed = 0.1f;
+ public float smoothRotationSpeed = 4f;
+ public float minimumOrthographicSize = 2.0f;
+ public float maximumOrthographicSize = 5.0f;
+
+ public float zoomSpeed = 1.0f;
+
+public float RotationIndex = 10f;
+
+private float targetOrtho;
+
+ void Start()
+ {
+     ResetCamera = Camera.main.transform.position;
+     targetOrtho = Camera.main.orthographicSize;
+ }
+
+void Update() {
+
+        if(Input.GetKey(KeyCode.Q)) {
+            turnAround(Vector2.zero,Camera.main.gameObject,Mathf.MoveTowards(0, RotationIndex, smoothRotationSpeed * Time.deltaTime));
+        }
+
+         if(Input.GetKey(KeyCode.E)) {
+            turnAround(Vector2.zero,Camera.main.gameObject,Mathf.MoveTowards(0, -RotationIndex, smoothRotationSpeed * Time.deltaTime));
+        }
+
+
+         float scroll = Input.GetAxis ("Mouse ScrollWheel");
+         if (scroll != 0.0f) {
+             targetOrtho -= scroll * zoomSpeed;
+             targetOrtho = Mathf.Clamp (targetOrtho, minimumOrthographicSize, maximumOrthographicSize);
+         }
+
+         Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
+            
+}
+ void LateUpdate()
+ {
+
+
+     if(Input.GetMouseButtonDown(0))
+     {
+         Origin = MousePos();
+     }
+     if (Input.GetMouseButton(0))
+     {
+         Diference = MousePos() - transform.position;
+         transform.position = Origin - Diference;
+     }
+     if (Input.GetMouseButton(1)) // reset camera to original position
+     {
+         transform.position = ResetCamera;
+     }
+ }
+ // return the position of the mouse in world coordinates (helper method)
+    Vector3 MousePos()
+    {
+     return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }  
+
+     public void turnAround (Vector2 point, GameObject obj, float angle)
+ {
+     Vector3 point3 = new Vector3(point.x,point.y,0);
+     Vector3 axis = new Vector3 (0,0,1);
+     obj.transform.RotateAround(point3,axis,angle);
+ }
+}
